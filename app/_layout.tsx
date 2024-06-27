f2a71beb71b3b5265347fca2b1ce854b17afe120
bugrarslan <bugrarsln7@gmail.com>
2024-06-27 14:46:40 +0300

    27.06 Clerk added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,30 +1,92 @@
-import { Ionicons } from "@expo/vector-icons"
-import { Stack, useRouter } from "expo-router"
-import { TouchableOpacity } from "react-native"
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
+import { Ionicons } from "@expo/vector-icons";
+import { Slot, SplashScreen, Stack, useRouter, useSegments } from "expo-router";
+import { TouchableOpacity } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as SecureStore from "expo-secure-store";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 
-export default function RootLayout() {
-	const router = useRouter();
+const CLERK_PUBLISHABLE_KEY = process.env
+  .EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY as string;
 
-	return (
-		<Stack>
-			<Stack.Screen
-				name="index"
-				options={{headerShown:false}}
-			/>
-			<Stack.Screen
-				name="login"
-				options={{
-					presentation:'modal',
-					title:'',
-					headerLeft: () => (
-						<TouchableOpacity
-							onPress={() => {router.back()}}
-						>
-							<Ionicons name='close-outline' size={28} />
-						</TouchableOpacity>
-					)
-				}}
-			/>
-		</Stack>
-	)
-}
+// Cache the Clerk JWT
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
+
+SplashScreen.preventAutoHideAsync();
+
+const InitialLayout = () => {
+  const router = useRouter();
+	const {isLoaded, isSignedIn} = useAuth();
+	const segments = useSegments();
+
+	const [loaded, error] = useFonts({
+		SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+	});
+
+	useEffect(() => {
+		if (error) throw error;
+	}, [error]);
+
+	useEffect(() => {
+		if (loaded) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded]);
+	
+	if (!isLoaded || !loaded) {
+		return <Slot/>
+	}
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="login"
+        options={{
+          presentation: "modal",
+          title: "",
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => {
+                router.back();
+              }}
+            >
+              <Ionicons name="close-outline" size={28} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+    </Stack>
+  );
+};
+
+const RootLayoutNav = () => {
+  return (
+    <ClerkProvider
+      publishableKey={CLERK_PUBLISHABLE_KEY}
+      tokenCache={tokenCache}
+    >
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <InitialLayout />
+      </GestureHandlerRootView>
+    </ClerkProvider>
+  );
+};
+
+export default RootLayoutNav;
