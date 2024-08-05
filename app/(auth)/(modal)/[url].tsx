325a0c9472ee45065aa6b837ae50d8c0535dbc4a
bugrarslan <bugrarsln7@gmail.com>
2024-08-05 22:23:04 +0300

    image bottomsheet created

--- app/(auth)/(modal)/[url].tsx
+++ app/(auth)/(modal)/[url].tsx
@@ -1,5 +1,11 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import React from "react";
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useCallback, useMemo, useRef } from "react";
 import { Stack, useLocalSearchParams } from "expo-router";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { ImageZoom } from "@likashefqet/react-native-image-zoom";
@@ -15,6 +21,8 @@ import {
   BottomSheetScrollView,
 } from "@gorhom/bottom-sheet";
 import * as Clipboard from "expo-clipboard";
+import Colors from "@/constants/Colors";
+import { defaultStyles } from "@/constants/Styles";
 
 const Page = () => {
   const { url, prompt } = useLocalSearchParams<{
@@ -22,6 +30,17 @@ const Page = () => {
     prompt?: string;
   }>();
   const { bottom } = useSafeAreaInsets();
+
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+  const snapPoints = useMemo(() => ["40%"], []);
+
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+
+  const handleCloseModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.dismiss();
+  }, []);
 
   const onCopyPrompt = () => {
     console.log("copy prompt");
@@ -36,8 +55,6 @@ const Page = () => {
       delay: 0,
     });
   };
-
-  const handlePresentModalPress = () => {};
 
   return (
     <RootSiblingParent>
@@ -106,6 +123,51 @@ const Page = () => {
             </View>
           </BlurView>
         </View>
+
+        <BottomSheetModal
+          ref={bottomSheetModalRef}
+          index={0}
+          snapPoints={snapPoints}
+          backgroundStyle={{ backgroundColor: Colors.grey }}
+          handleIndicatorStyle={{ backgroundColor: Colors.greyLight }}
+        >
+          <View
+            style={[styles.modalContainer, { paddingBottom: bottom, flex: 1 }]}
+          >
+            <BottomSheetScrollView>
+              <View style={{ flex: 1 }}>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Text style={styles.titleText}>Prompt</Text>
+                  <Pressable
+                    onPress={handleCloseModalPress}
+                    style={styles.closeBtn}
+                  >
+                    <Ionicons
+                      name="close-outline"
+                      size={24}
+                      color={Colors.greyLight}
+                    />
+                  </Pressable>
+                </View>
+                <Text style={styles.promptText}>{prompt}</Text>
+              </View>
+            </BottomSheetScrollView>
+            <TouchableOpacity
+              style={[
+                defaultStyles.btn,
+                { backgroundColor: "#fff", marginTop: 16 },
+              ]}
+              onPress={onCopyPrompt}
+            >
+              <Text style={styles.buttonText}>Copy</Text>
+            </TouchableOpacity>
+          </View>
+        </BottomSheetModal>
       </BottomSheetModalProvider>
     </RootSiblingParent>
   );
@@ -139,5 +201,33 @@ const styles = StyleSheet.create({
     color: "#fff",
     fontSize: 12,
     paddingTop: 6,
+  },
+  modalContainer: {
+    flex: 1,
+    paddingHorizontal: 24,
+  },
+  titleText: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  promptText: {
+    color: "#fff",
+    fontSize: 16,
+  },
+  buttonText: {
+    color: Colors.grey,
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: "500",
+  },
+  closeBtn: {
+    backgroundColor: Colors.dark,
+    borderRadius: 20,
+    height: 26,
+    width: 26,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
